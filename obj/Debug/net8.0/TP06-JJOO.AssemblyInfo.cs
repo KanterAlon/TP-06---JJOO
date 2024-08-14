9f206ba27891c6858ed554672fd3a176456a6b73
Kanter Alon <95320323@est.ort.edu.ar>
2024-08-14 12:24:44 -0300

    Falta Stored Procedures y Views

--- obj/Debug/net8.0/TP06-JJOO.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP06-JJOO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP06-JJOO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e89a00222bc36962ba89c7d5320d9a09b207d43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a29cc8146cd7c5e3c70ca45ac7d0b1e58c27ae01")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP06-JJOO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP06-JJOO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
